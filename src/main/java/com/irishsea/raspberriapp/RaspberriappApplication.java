25c7ff438138db6e488e98c437b8c395a280c51e
Stefania Zh <irishsea>
2021-02-27 02:54:37 +0700

    Initial commit

---
+++ src/main/java/com/irishsea/raspberriapp/RaspberriappApplication.java
@@ -0,0 +1,13 @@
+package com.irishsea.raspberriapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberriappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspberriappApplication.class, args);
+	}
+
+}
